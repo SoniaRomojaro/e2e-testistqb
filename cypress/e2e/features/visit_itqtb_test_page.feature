aeea2d1951bfd059bbdc04519951ed695dee65bc
SoniaRomojaro <soniaromojaro@gmail.com>
2024-06-21 21:11:37 +0200

    welcome

--- cypress/e2e/features/visit_itqtb_test_page.feature
+++ cypress/e2e/features/visit_itqtb_test_page.feature
@@ -5,4 +5,7 @@ Feature: Visit Istqb Test Page
 Scenario: Visit istqb test page 
     Given the student visit istqb test page
     When the student click on welcome menu
-    Then the url shold include welcome
+    Then the istqb test page url should include welcome
+    And the istqb test page title should be equal to ISTQB ONLINE TEST
+    And the istqb menu should have 3 elements
+    And the istqb page should have header title ISTQB Online Test
--- cypress/e2e/step_definitions/visit_istqb_test_page_steps.js
+++ cypress/e2e/step_definitions/visit_istqb_test_page_steps.js
@@ -5,80 +5,31 @@ import {
 } from "@badeball/cypress-cucumber-preprocessor";
 
 
+
 Given("the student visit istqb test page", () => {
-  //Visitar una pagina web diferente a la que este en base url
-  //cy.visit("https://bootcampqa.com/");
-
-  //Cuando esta definida en cypress.config.js la baseurl 
-  cy.visit("/");
-
-  //borrar todas las cookies. Por defecto ya se borran en cypress antes de iniciar el test por lo que no suele ser necesario usarlo.
-  cy.clearCookies();
-
-  //recargar la pagina
-  cy.reload();
-
-
+    cy.visit("/");
 });
 
-When("the student click on welcome menu", () => {
-  //coger un elemento de la pagina con get
-  //1. Mas recomendada por data-test o data-cy
-  cy.get('[data-cy="menu-welcome"]').click();
-  //2. Por texto, lo segundo mas recomendado
-  cy.contains("Welcome").click();
-  //hacer click el titulo h2 con la palabra welcome
-  cy.get('h2').contains("Welcome").click();
 
-  //3. Algo menos recomendada pero bien, por id
-  cy.get('#menu-welcome-id').click();
-  //4. por clase (no recomendada)
-  cy.get(".nav-link").first().click(); //el primer elemento con class=nav-link
-  cy.get(".nav-link").filter('[href="#welcome"]').click(); //elemento con class=nav-link que ademas tenga atributo href=#welcome
-  //5.por tipo de elemento (no recomendada, esta la que menos de todas)
-  cy.get('a').first().click(); //haz click en el primer elemento de tipo a
-
- //HACER CLICK DERECHO
- cy.get('[data-cy="menu-welcome"]').rightclick();
-
- //HACER DOBLE CLICK
- cy.get('[data-cy="menu-welcome"]').dblclick();
+  When("the student click on welcome menu", () => {
+    cy.get('[data-cy="menu-welcome"]').click();
 });
 
 Then("the istqb test page url should include welcome", () => {
-  //la url debe tener el texto indicado
-  cy.url().should('includes','#welcome');
-  //la url debe ser exactamente igual al texto indicado
-  cy.url().should('eq','https://bootcamp-qa.github.io/istqb-online-test/#welcome');
-});
+   cy.url().should('includes','#welcome');
+ });
 
 Then("the istqb test page title should be equal to ISTQB ONLINE TEST", () => {
-  //la url debe tener el texto indicado
-  cy.title().should('includes','ISTQB');
-  //la url debe ser exactamente igual al texto indicado
-  cy.title().should('eq','ISTQB ONLINE TEST');
+    cy.title().should('includes','ISTQB');
+ });
+
+ Then("the istqb menu should have 3 elements", () => {
+    cy.get('[data-cy*="menu-"]').should('have.length',3);
 });
-
-Then("the istqb menu should have 3 elements", () => {
-  //comprueba que hay en total 3 elementos en el menu (los elementos en el menu tienen todos data-cy menu-)
-  cy.get('[data-cy*="menu-"]').should('have.length',3);
-  //comprueba que en el menu hay mas de dos elementos
-  cy.get('[data-cy*="menu-"]').should('have.length.greaterThan',2);
-  //comprueba que en el menu hay menus de 4 elementos
-  cy.get('[data-cy*="menu-"]').should('have.length.lessThan',4);
-  //comprueba que en el menu no hay 1 solo elemento
-  cy.get('[data-cy*="menu-"]').should('not.have.length',1);
-});
-
-
 
 Then("the istqb page should have header title ISTQB Online Test", () => {
-  //COMPRUEBA QUE EL ELEMENTO ES VISIBLE
-  cy.contains("ISTQB Online Test").should('be.visible');
-  //COMPRUEBA QUE EL ELEMENTO TIENE EL TEXTO EXACTO
-  cy.contains("ISTQB Online Test").should('have.text', 'ISTQB Online Test');
-  //COMPRUEBA QUE EL ELEMENTO CONTIENE EL TEXTO
-  cy.contains("ISTQB Online Test").should('include.text', 'ISTQB');
-  //COMPRUEBA QUE EL ELEMENTO NO CONTIENE EL TEXTO
-  cy.contains("ISTQB Online Test").should('not.contain', 'Bootcamp');
+    cy.contains("ISTQB Online Test").should('be.visible');
+    cy.contains("ISTQB Online Test").should('have.text', 'ISTQB Online Test');
+    cy.contains("ISTQB Online Test").should('include.text', 'ISTQB');
+    cy.contains("ISTQB Online Test").should('not.contain', 'Bootcamp');
 });
